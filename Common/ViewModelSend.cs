e2adfc0df49413c70cd44f3bfd2f173cb941191a
DikiYLexa <dubovan36@gmail.com>
2025-11-22 15:11:20 +0500

    ViewModelSend. Chechelnitskiy

--- Common/ViewModelSend.cs
+++ Common/ViewModelSend.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Common
 {
-    internal class ViewModelSend
+    public class ViewModelSend
     {
+        public string Message {  get; set; }
+        public int Id { get; set; }
+        public ViewModelSend(string message, int Id) 
+        { 
+            this.Message = message;
+            this.Id = Id;
+        }
     }
 }
