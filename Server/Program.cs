aaab2817023e0f74be2d7b6b4d95c07c94f0bdcf
DikiYLexa <dubovan36@gmail.com>
2025-11-22 15:30:18 +0500

    AutorizationUser.Chechelnitskiy

---
+++ Server/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+   
+    public class Program
+    {
+        public static List<User> Users = new List<User>();
+        public static IPAddress IpAdress;
+        public static int Port;
+        public static bool AutorizationUser(string login, string password)
+        {
+            User user = null;
+            user = Users.Find(x => x.login == login && x.password == password);
+            return user != null;
+
+        }
+    }
+}
